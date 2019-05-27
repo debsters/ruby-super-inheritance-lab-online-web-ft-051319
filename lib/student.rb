6223beb2dc2d169e1fc67bacb8bfb0cc72bccb74
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-27 23:36:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,10 @@
-require_relative "./student.rb"
-class ChattyStudent < Student
 
+class Student
   def hello
-      super
-      puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    10.times {super}
+    puts "Pick me!"
   end
-
-	# def raise_hand
-  #   10.times do
-  #     super
-  #   end
-  # end
-
 end
